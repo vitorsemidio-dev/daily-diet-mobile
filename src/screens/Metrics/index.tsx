eb13d32e8c8cd5bfd12c9ed659377e513aed5946
Vitor Emídio <vitorsemidio.96@gmail.com>
2023-04-26 22:37:55 -0300

    feat: layout screen metrics

--- src/screens/Metrics/index.tsx
+++ src/screens/Metrics/index.tsx
@@ -1,9 +1,62 @@
-import { Container, Text } from './styles';
+import { Text, Title } from '@components/Typography';
+import { Feather } from '@expo/vector-icons';
+import {
+  Container,
+  DataMetricsContainer,
+  InfoContainer,
+  MetricsContainer,
+} from './styles';
+import { useTheme } from 'styled-components';
+import { useNavigation } from '@react-navigation/native';
+import { Banner } from '@components/Banner';
 
 export function Metrics() {
+  const theme = useTheme();
+  const navigate = useNavigation();
+
+  function goBack() {
+    navigate.goBack();
+  }
+
   return (
     <Container>
-      <Text>Metrics Daily Diet | With Styled Components</Text>
+      <Banner variant="success" style={{ height: 200 }}>
+        <Feather
+          name="arrow-left"
+          size={24}
+          color={theme.COLORS.BRAND_GREEN_DARK}
+          style={{ position: 'absolute', left: 24, top: '33%' }}
+          onPress={goBack}
+        />
+        <Title size="lg">90,86%</Title>
+        <Text>das refeições dentro da dieta</Text>
+      </Banner>
+
+      <MetricsContainer>
+        <Title size="sm" style={{ textAlign: 'center' }}>
+          Estatíticas gerais
+        </Title>
+        <DataMetricsContainer>
+          <Banner>
+            <Title>22</Title>
+            <Text>melhor sequência de pratos dentro da dieta dieta</Text>
+          </Banner>
+          <Banner>
+            <Title>109</Title>
+            <Text>refeições registradas</Text>
+          </Banner>
+          <InfoContainer>
+            <Banner variant="success" style={{ width: '48.5%' }}>
+              <Title>99</Title>
+              <Text align="center">refeições dentro da dieta</Text>
+            </Banner>
+            <Banner variant="danger" style={{ width: '48.5%' }}>
+              <Title>10</Title>
+              <Text align="center">refeições fora da dieta</Text>
+            </Banner>
+          </InfoContainer>
+        </DataMetricsContainer>
+      </MetricsContainer>
     </Container>
   );
 }
--- src/screens/Metrics/styles.ts
+++ src/screens/Metrics/styles.ts
@@ -2,12 +2,23 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
   flex: 1;
-  background-color: #000;
-  align-items: center;
-  justify-content: center;
 `;
 
-export const Text = styled.Text`
-  font-size: 20px;
-  color: #fff;
+export const MetricsContainer = styled.View`
+  flex: 1;
+  padding: 32px 24px;
+  background-color: ${({ theme }) => theme.COLORS.BASE_GRAY_700};
+  margin-top: -24px;
+  border-radius: 20px;
+`;
+
+export const DataMetricsContainer = styled.View`
+  margin-top: 32px;
+  gap: 16px;
+`;
+
+export const InfoContainer = styled.View`
+  display: flex;
+  flex-direction: row;
+  gap: 12px;
 `;
